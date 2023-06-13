5243bab3de4c981f82c56c5b8adbb5d115a91780
hunny-nik <106017090+hunny-nik@users.noreply.github.com>
2023-06-13 11:10:54 +0500

    usersecussfull

--- routes/user.js
+++ routes/user.js
@@ -6,6 +6,7 @@ Router.post("/addUser", controller.addUser);
 Router.post("/loginUser", controller.login);
 Router.get("/getAll", controller.getAll);
 Router.get("/getOne/:id", controller.getOne);
+Router.delete("/deleteUser/:id", controller.deleteUser);
 Router.get("/updateUser/:id", controller.updateUser);
 Router.put("/updatePassword/:id", controller.updatePassword);
 Router.put("/updateImage/:id", upload.single("file"), controller.updateImage);
